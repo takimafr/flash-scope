135d07ecd5e00892500fef7f03d27b22f6234ef6
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-04-04 16:43:24 +0200

    Implemented the Fluid API FlashScopeHandler.bind(name).to(value);

--- src/main/java/com/excilys/utils/web/flash/FlashScopeHandler.java
+++ src/main/java/com/excilys/utils/web/flash/FlashScopeHandler.java
@@ -27,6 +27,35 @@ import java.util.Map;
 // NotThreadSafe
 public class FlashScopeHandler {
 
+	public static final class Binder {
+
+		private final String name;
+
+		private Binder(String name) {
+			this.name = name;
+		}
+
+		/**
+		 * @param value The value to be set for the associated attribute name
+		 */
+		public void to(Object value) {
+			setFlashAttribute(name, value);
+		}
+	}
+	
+	/**
+	 * Call this method to set flash attributes. The fluid API should be used
+	 * the following way : {@link FlashScopeHandler}.bind(name).to(value);
+	 * 
+	 * @param name
+	 *            the flash attribute name
+	 * @return a {@link Binder} on which you can call the
+	 *         {@link Binder#to(Object)} method to set the attribute value.
+	 */
+	public static Binder bind(String name) {
+		return new Binder(name);
+	}
+
 	// TODO handle InheritableThreadLocal
 	private static final ThreadLocal<Map<String, Object>> HOLDER = new ThreadLocal<Map<String, Object>>();
 
@@ -48,4 +77,5 @@ public class FlashScopeHandler {
 	public static void setFlashAttribute(String name, Object value) {
 		getFlash().put(name, value);
 	}
+
 }
