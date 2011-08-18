be16005fcad4d881e6cb3f25132e3aa89ff34df7
Stephane Landelle <slandelle+github@excilys.com>
2011-08-18 15:00:23 +0200

    Refactoring in order to introduce Spring support

--- src/main/java/com/excilys/ebi/utils/web/flash/FlashFilter.java
+++ src/main/java/com/excilys/ebi/utils/web/flash/FlashFilter.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010-2011 eBusiness Information, Groupe Excilys (www.excilys.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,69 +16,74 @@
 package com.excilys.ebi.utils.web.flash;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.lang.reflect.Constructor;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /**
- * Filter in charge of Synchronizing the flash scope in Session and the one in
- * the ThreadLocal.
- * 
+ * Filter in charge of synchronizing the flash scope and the HTTP session.
  * 
  * @author <a href="mailto:slandelle@excilys.com">Stephane LANDELLE</a>
  */
-public class FlashScopeFilter extends AbstractFilter {
+public class FlashFilter implements Filter {
 
-	/**
-	 * The key to store the Flash in HTTP Session
-	 */
-	private static final String FLASH_SCOPE_KEY = "com.excilys.utils.web.flash";
+	public static final String INIT_PARAM = "excilys.flash.storage";
+
+	private Storage storage;
+
+	private FlashScopeHandler handler;
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+		String storageName = filterConfig.getInitParameter(INIT_PARAM);
+
+		if ((storageName == null) || "".equals(storageName)) {
+			// Set default
+			storage = BuiltInStorages.MODE_THREADLOCAL;
+
+		} else {
+			storage = BuiltInStorages.getBuiltIn(storageName);
+		}
+
+		if (storage == null) {
+			// Try to load a custom storage
+			try {
+				Class<?> clazz = Class.forName(storageName);
+				Constructor<?> customStrategy = clazz.getConstructor();
+				storage = Storage.class.cast(customStrategy.newInstance());
+
+			} catch (Exception ex) {
+				throw new ExceptionInInitializerError(ex);
+			}
+		}
+
+		if (storage == null) {
+			throw new ExceptionInInitializerError("Impossible to find a strategy for " + storageName);
+		}
+
+		handler = new FlashScopeHandler(storage);
+	}
+
+	public void destroy() {
+		// nopo
+	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+		HttpServletRequest httpRequest = HttpServletRequest.class.cast(request);
+
 		try {
-			movePreviousFlashFromSessionToRequest(request);
+			handler.movePreviousFlashFromSessionToRequest(httpRequest);
 			chain.doFilter(request, response);
 
 		} finally {
-			storeCurrentFlashInSession(request);
+			handler.storeCurrentFlashInSession(httpRequest);
 		}
-	}
-
-	/**
-	 * Transform the content of the previous Flash as request attributes
-	 * 
-	 * @param request
-	 *            the request
-	 */
-	private void movePreviousFlashFromSessionToRequest(ServletRequest request) {
-		HttpSession session = HttpServletRequest.class.cast(request).getSession();
-		@SuppressWarnings("unchecked")
-		Map<String, Object> flash = Map.class.cast(session.getAttribute(FLASH_SCOPE_KEY));
-		if (flash != null) {
-			for (Entry<String, Object> attribute : flash.entrySet()) {
-				request.setAttribute(attribute.getKey(), attribute.getValue());
-			}
-			session.setAttribute(FLASH_SCOPE_KEY, null);
-		}
-	}
-
-	/**
-	 * Store the current Flash in the HTTP Session so that it can be retrieved
-	 * in the next Request
-	 * 
-	 * @param request
-	 *            the request
-	 */
-	private void storeCurrentFlashInSession(ServletRequest request) {
-		HttpSession session = HttpServletRequest.class.cast(request).getSession();
-		session.setAttribute(FLASH_SCOPE_KEY, FlashScope.getFlash());
-		FlashScope.clear();
 	}
 }
